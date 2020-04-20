464c36c027092865223ba41bc30cd3e41aab5813
Alex Shekunsky <oleksandr.shchekunskyi@windmill.ch>
2020-04-20 13:34:53 +0300

    Initial Commit

---
+++ PopularMoviesTestApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  PopularMoviesTestApp
+//
+//  Created by Alex2 on 20.04.2020.
+//  Copyright © 2020 Alex2. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PopularMoviesTestApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PopularMoviesTestApp
+//
+//  Created by Alex2 on 20.04.2020.
+//  Copyright © 2020 Alex2. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
